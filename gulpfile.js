6774543c3ede4b2cf607d9dd1fa1ea527a06e55b
Infacq <muhaimincs@itronics.com.my>
2015-06-01 16:01:43 +0800

    prepare copy statement

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,20 @@ elixir(function(mix) {
     mix
 		.bower()
 		.scripts()
-		.copy('resources/assets/vendor/material-design-icons/action', 'public/img/action')
-		.phpUnit();
+		//.copy('resources/assets/vendor/material-design-icons/action', 'public/img/action')
+		//.copy('resources/assets/vendor/material-design-icons/alert', 'public/img/alert')
+		//.copy('resources/assets/vendor/material-design-icons/av', 'public/img/av')
+		//.copy('resources/assets/vendor/material-design-icons/communication', 'public/img/communication')
+		//.copy('resources/assets/vendor/material-design-icons/content', 'public/img/content')
+		//.copy('resources/assets/vendor/material-design-icons/device', 'public/img/device')
+		//.copy('resources/assets/vendor/material-design-icons/editor', 'public/img/editor')
+		//.copy('resources/assets/vendor/material-design-icons/file', 'public/img/file')
+		//.copy('resources/assets/vendor/material-design-icons/hardware', 'public/img/hardware')
+		//.copy('resources/assets/vendor/material-design-icons/image', 'public/img/image')
+		.copy('resources/assets/vendor/material-design-icons/maps', 'public/img/maps');
+		//.copy('resources/assets/vendor/material-design-icons/navigation', 'public/img/navigation')
+		//.copy('resources/assets/vendor/material-design-icons/notification', 'public/img/notification')
+		//.copy('resources/assets/vendor/material-design-icons/social', 'public/img/social')
+		//.copy('resources/assets/vendor/material-design-icons/sprites', 'public/img/sprites')
+		//.copy('resources/assets/vendor/material-design-icons/toggle', 'public/img/toggle');
 });
