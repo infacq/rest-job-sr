7724c6cd2fe598b36becd7bc899b26abd4baa56a
Infacq <muhaimincs@itronics.com.my>
2015-06-05 16:59:41 +0800

    bismillah

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,17 +2,18 @@
 <html lang="en">
 <html>
 	<head>
-		<title>Merantau</title>
-		<link href="/css/vendor.css" rel="stylesheet" type="text/css">
+		<title>Service Rocket Job Application</title>
+		<meta name="csrf-token" content="{!! csrf_token() !!}">
+		<link href="{{ asset('/css/vendor.css') }}" rel="stylesheet" type="text/css">
 	</head>
-	<body ng-app="getLostApp" ng-cloak>
+	<body ng-app="serviceRocketApp" ng-cloak>
 		<md-content>
 			<md-toolbar>
 				<div class="md-toolbar-tools">
 				<md-button class="md-icon-button" aria-label="Settings">
-					<md-icon md-svg-icon="/img/maps/svg/design/ic_local_airport_24px.svg"></md-icon>
+					<md-icon md-svg-icon="/img/logo.jpg"></md-icon>
 				</md-button>
-				<h2><span class="md-title">Get Lost!</span></h2>
+				<h2><span class="md-title">Service Rocket</span></h2>
 				</div>
 			</md-toolbar>
 			{{-- The content area --}}
@@ -23,67 +24,38 @@
 				<md-card flex ng-controller="MainCtrl as ctrl">
 					<md-card-content>
 						<!-- The row for the city select autocomplete list -->
-						<md-content>
-							<md-autocomplete  md-selected-item="ctrl.selectedItem"
-								md-search-text="ctrl.searchText"
-								md-items="item in ctrl.querySearch(ctrl.searchText)"
-								md-item-text="item.display"
-								md-min-length="0"
-								placeholder="Where are you flying from?">
-								<md-item-template>
-									<span md-highlight-text="ctrl.searchText" md-highlight-flags="^i">@{{item.display}}</span>
-								</md-item-template>
-								<md-not-found>
-									No matches found for "@{{ctrl.searchText}}".
-								</md-not-found>
-							</md-autocomplete>
-							<md-tooltip>
-								Pick the city you want to start from!
-							</md-tooltip>
-							<p class="md-caption">NYC, CHI and LON work well</p>
+						<md-content layout-padding="">
+							{!! Form::open(['name' => 'checkoutForm']) !!}
+								<md-input-container flex="" class="md-icon-float">
+									<label>Name</label>
+									<md-icon md-svg-icon="/img/action/svg/production/ic_face_48px.svg"></md-icon>									
+									{!! Form::text('appname', null, ['ng-model' => 'applicant.name']); !!}
+								</md-input-container>
+								<md-input-container flex="" class="md-icon-float">
+									<label>Email</label>
+									<md-icon md-svg-icon="/img/communication/svg/production/ic_quick_contacts_mail_48px.svg"></md-icon>
+									{!! Form::email('appemail', null, ['ng-model' => 'applicant.email']); !!}
+								</md-input-container>
+								<md-input-container flex="">
+									<label>Biography</label>
+									{!! Form::textarea('appbio', null, ['ng-model' => 'applicant.about', 'md-maxlength' => '150', 'columns' => '1']); !!}
+								</md-input-container>							
+								<md-button>
+									{!! Form::label('appdocs', 'Resume'); !!}									
+								</md-button>
+								{!! Form::file('appdocs'); !!}
+							{!! Form::close() !!}
 						</md-content>
-						<!-- The row for the budget and dates -->
-						<div layout>
-							<md-input-container flex>
-								<md-select placeholder="Set your budget" ng-model="info.maxfare">
-									<div ng-repeat="p in prices">
-										<md-option value="@{{p.value}}">@{{p.show}}</md-option>
-									</div>
-								</md-select>
-							</md-input-container>
-							<md-input-container flex>
-								<label>Start date</label>
-								<input type="date" ng-model="info.departuredate">
-							</md-input-container>
-							<md-input-container flex>
-								<label>End date</label>
-								<input type="date" ng-model="info.returndate">
-							</md-input-container>
-						</div>
 						{{-- The submit button --}}
 						<div layout>
 							<md-button class="md-raised md-primary" ng-click="submit()">Search for destinations!</md-button>
 						</div>
-						<!-- The results -->
-						<md-content ng-show="fareinfo">
-							<md-list>
-								<md-subheader class="md-no-sticky">Results</md-subheader>
-								<md-list-item class="md-3-line" ng-repeat="item in fareinfo | orderBy:'LowestFare'">
-									<div class="md-list-item-text">
-										<h3>Destination: @{{ item.DestinationLocation }}</h3>
-										<h4>Lowest fare: RM @{{ item.LowestFare }}</h4>
-										<p>Lowest non-stop fare: RM @{{ item.LowestNonStopFare }}</p>
-									</div>
-									<md-divider></md-divider>
-								</md-list-item>
-							</md-list>
-						</md-content>
 					<md-card-content>
 				</md-card>
 				<div flex="20"></div>
 			</div>
 		</md-content>
-	<script src="/js/vendor.js"></script>
-	<script src="/js/all.js"></script>
+	<script src="{{ asset('/js/vendor.js') }}"></script>
+	<script src="{{ asset('/js/all.js') }}"></script>
 	</body>
 </html>
