e443c8ee2d2b260e85ea6735285667e23b14b67e
Infacq <muhaimincs@itronics.com.my>
2015-06-15 16:05:00 +0800

    remove unnecessary

--- public/js/all.js
+++
@@ -1,9 +0,0 @@
-angular
-	.module('serviceRocketApp', ['ngMaterial'])
-	.controller('MainCtrl', function($rootScope, $scope, $mdToast, $animate, $http, $timeout, $q, $log) {
-		'use strict';
-		// Initialize the scope variables
-		$scope.applicant = {};
-
-	});
-//# sourceMappingURL=all.js.map
