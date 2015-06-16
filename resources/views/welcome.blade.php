b46f0636d205b829b6057d9178d3b379b26a2a51
Infacq <muhaimincs@itronics.com.my>
2015-06-16 09:45:48 +0800

    rename css resources

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,54 +3,46 @@
 	<head>
 		<title>Service Rocket Job Application</title>
 		<meta name="csrf-token" content="{!! csrf_token() !!}">
-		<link href="{{ asset('/css/vendor.css') }}" rel="stylesheet" type="text/css">
+		<link href="{{ asset('/css/all.css') }}" rel="stylesheet" type="text/css">
 	</head>
 	<body ng-app="jobApp" ng-cloak>
-		<md-content>
-			<md-toolbar>
-				<div class="md-toolbar-tools">
-				<md-button class="md-icon-button">
-					<md-icon md-svg-icon="/img/rocket.svg"></md-icon>
-				</md-button>
-				<h2><span class="md-title">Service Rocket</span></h2>
-				</div>
-			</md-toolbar>
-			{{-- The content area --}}
-			<div layout="row">
-			{{-- 20% padding to the left --}}
-				<div flex="20"></div>
-				{{-- The main action happens here --}}
-				<md-card flex ng-controller="MainCtrl as ctrl">
-					<md-card-content>
-						<!-- The row for the city select autocomplete list -->
-						<md-content layout-padding="">
-							{!! Form::open(['name' => 'checkoutForm', 'url' => 'http://sr-recruit.herokuapp.com/resumes', 'enctype' => 'multipart/form-data']) !!}
-								<md-input-container flex="" class="md-icon-float">
-									<label>Name</label>
-									<md-icon md-svg-icon="/img/action/svg/production/ic_face_48px.svg"></md-icon>									
-									{!! Form::text('name', null, ['ng-model' => 'appname']); !!}
-								</md-input-container>
-								<md-input-container flex="" class="md-icon-float">
-									<label>Email</label>
-									<md-icon md-svg-icon="/img/communication/svg/production/ic_quick_contacts_mail_48px.svg"></md-icon>
-									{!! Form::email('email', null, ['ng-model' => 'appemail']); !!}
-								</md-input-container>
-								<md-input-container flex="">
-									<label>Biography</label>
-									{!! Form::textarea('about', null, ['ng-model' => 'appabout', 'md-maxlength' => '150']); !!}
-								</md-input-container>							
-									<input type="file" name="file">
-								<md-button>
-								{!! Form::submit('Send '); !!}
-								</md-button>
-							{!! Form::close() !!}
-						</md-content>
-						{{-- The submit button --}}
-					</md-card-content>
-				</md-card>
-				<div flex="20"></div>
-			</div>
-		</md-content>
+		<div layout="row" layout-align="center center">
+			{{-- The main action happens here --}}
+			<md-card flex="33">
+				<img ng-src="https://lh3.googleusercontent.com/-i2TYuCl4Iy4/VNV7muUt3sI/AAAAAAAAAAU/xqhMjZS4fgk/w1001-h563-no/" class="md-card-image" alt="Washed Out">
+				<md-card-content>					
+					<!-- The row for the city select autocomplete list -->
+					<md-content layout-padding="">
+						{!! Form::open(['name' => 'checkoutForm', 'url' => 'http://sr-recruit.herokuapp.com/resumes', 'enctype' => 'multipart/form-data']) !!}
+							<md-input-container flex="" class="md-icon-float">
+								<label>Name</label>
+								<md-icon md-svg-icon="/img/action/svg/production/ic_face_48px.svg"></md-icon>									
+								{!! Form::text('name', null, ['ng-model' => 'appname']); !!}
+							</md-input-container>
+							<md-input-container flex="" class="md-icon-float">
+								<label>Email</label>
+								<md-icon md-svg-icon="/img/communication/svg/production/ic_quick_contacts_mail_48px.svg"></md-icon>
+								{!! Form::email('email', null, ['ng-model' => 'appemail']); !!}
+							</md-input-container>
+							<md-input-container flex="">
+								<label>Biography</label>
+								{!! Form::textarea('about', null, ['ng-model' => 'appabout', 'md-maxlength' => '150']); !!}
+							</md-input-container>							
+							<md-button class="md-fab" aria-label="Upload resume">
+								<label for="file-input">
+									<md-icon md-svg-icon="/img/action/svg/production/ic_assignment_48px.svg" />
+								</label> 
+							</md-button> Attach your resume								
+							<input type="file" name="file" id="file-input" style="display: none">
+							<md-button class="md-fab main" aria-label="Submit application">
+								<md-icon md-svg-icon="/img/content/svg/production/ic_send_48px.svg"></md-icon>
+							</md-button>								
+						{!! Form::close() !!}
+					</md-content>
+					{{-- The submit button --}}
+				</md-card-content>
+			</md-card>
+		</div>
 		<pre>@{{log}}</pre>
 	<script src="{{ asset('/js/bundle.js') }}"></script>
 	</body>
