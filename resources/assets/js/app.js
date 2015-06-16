f683b215b70bc9310d2456ca4e9918cc81ec68e4
Infacq <muhaimincs@itronics.com.my>
2015-06-16 12:44:44 +0800

    finding to output file selected

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,6 +3,9 @@ angular
 	.module('jobApp', [require('angular-material')])
 	.controller('MainCtrl', ['$rootScope', '$scope', '$mdToast', '$animate', '$http', '$timeout', '$q', '$log',
 		function($rootScope, $scope, $mdToast, $animate, $http, $timeout, $q, $log) {
-		'use strict';
-		
+		'use strict';		
+		// Initialize the scope variables
+		$scope.$watch('file', function () {
+			console.log($scope.file);
+		});
 	}]);
